4b76b977568affa45126b8b041a931f0e1cfbedc
Mattis49 <mattisbbn@gmail.com>
2024-02-29 12:19:04 +0100

    Premier commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Code
+function loadContent(){
+    let div = document.getElementsByClassName("container")[0];
+    let divClone = div.cloneNode(true);
+    let loading = document.getElementsByClassName("loading")[0];
+    document.body.appendChild(divClone);
+    document.body.insertBefore(divClone, loading);
+}
+window.addEventListener("scroll", () => {
+    const {scrollHeight, clientHeight, scrollTop} = document.documentElement
+    if(scrollTop + clientHeight >= scrollHeight - 1){
+      loadContent()
+
+    }
+})
